c2450c03eaf5ff3e3beac72a47fcd76d6a19dbc4
Viacheslav Urdzik <viacheslav.urdzik@indeema.com>
2021-03-09 13:13:37 +0200

    Add rect function

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,61 +1,154 @@
-import java.util.function.DoubleFunction
+import java.util.regex.Pattern
 
-//fun main(args: Array<String>) {
-////    val matrix = arrayOf(doubleArrayOf(-2.0, -4.0, 0.0, -6.0), doubleArrayOf(1.0, 2.0, -6.0, 0.0), doubleArrayOf(-2.0, 4.0, -6.0, -4.0))
-//
-////    val result = Taculator.nDeriv(
-//////        arrayOf(1,3,4,8),
-//////        arrayOf(1,2,2,5)
-////    )
-////    println(result)
-//}
-
-
-// approximate the limit
-private const val DX = 0.0001
-
-/**
-
- */
-private fun derive(f: DoubleFunction<Double>): DoubleFunction<Double> {
-    return DoubleFunction { x: Double ->
-        (f.apply(x + DX) - f.apply(
-            x
-        )) / DX
-    }
-}
 
 fun main(args: Array<String>) {
-    val one = "15º"
-    val two = "15º1'"
-    val three = "15.60°58.7'59\""
-    val result = Taculator.rect(
-        "3e^pi/2"
-    )
-    println(result)
 
 
-//        val x = DerivativeStructure(1, 3, 0, 1.0)
+//    var mainStr = "\\frac{\\frac{1+1}{20+20}}{\\frac{10+10}{200+200}}+\\frac{3+3}{4+4}"
+//    val result = mainStr.findAllStartEndIndexes("\\frac{", "}", 2)
 //
-//        println(x.value)
-//        // Basically, x --> x^2.
-//        // Basically, x --> x^2.
-//        val x2: DerivativeStructure = x.pow(2)
-//        //Linear combination: y = 4x^2 +
-//
-//        /*Scanner scan = new Scanner(System.in);
-//    System.out.println("Input function:");
-//    String function=scan.nextLine();*/
-//
-//        //Linear combination: y = 4x^2 + 2x
-//
-//        /*Scanner scan = new Scanner(System.in);
-//    System.out.println("Input function:");
-//    String function=scan.nextLine();*/
-//        val y = DerivativeStructure(4.0, x2, 2.0, x)
-//        println("y    = " + y.value)
-//        println("y'   = " + y.getPartialDerivative(1))
-//        println("y''  = " + y.getPartialDerivative(2))
-//        println("y''' = " + y.getPartialDerivative(3))
+//    val mutableList = mutableListOf<Pair<String, String>>()
+//    result.reversed().forEach {
+//        mutableList.add(pair( mainStr.getFromTo(it.first, it.second)))
+//    }
+//    mutableList.reversed().forEach {
+//     mainStr =  mainStr.replace(it.first, it.second)
+//    }
 
+    val mainStr = "\\frac{\\frac{1+1}{20+20}}{\\frac{10+10}{200+200}}"
+    println(test(mainStr))
+//    frac()
+
+}
+
+//private fun frac() {
+//    val list = mutableListOf<String>()
+//
+//    var result = "\\frac{1+1}{2+2} + \\frac{3+3}{4+4}"
+//
+//
+//
+//    result.split("+", "-", "*").forEach {
+//        var command = it
+//        while (true) {
+//            if (command.contains("\\frac{")) {
+//                command = pair(command)
+//            } else {
+//                break
+//            }
+//        }
+//        if (command != "\\frac") {
+//            list.add(command)
+//        }
+//    }
+//
+//    System.out.println(
+//        "Result = ${list.joinToString("").replace(" ", "")}"
+//    )
+//}
+
+private fun test(str: String): String {
+    var str = str
+    var first = ""
+    var second = ""
+    val ext = Pattern.compile("\\\\frac\\{(.*)")
+    val p = str.split(ext)
+//    val m = p.matcher(str)
+//    while (m.find()) {
+//        first = m.group(0)
+//        second = m.group(0)
+//        second = second.removePrefix("{")
+//        second = second.removeSuffix("}")
+//    }
+    return p.joinToString { it }
+}
+
+private fun pair(str: String): Pair<String, String> {
+    var first = ""
+    var second = ""
+    val p = Pattern.compile("(\\\\frac\\{)(.*)\\}(\\{(.*)\\})")
+    val m = p.matcher(str)
+    while (m.find()) {
+        first = m.group(2)
+        second = m.group(3)
+        second = second.removePrefix("{")
+        second = second.removeSuffix("}")
+    }
+    return Pair("\\frac{$first}{$second}", "($first)/($second)")
+}
+
+private fun String.getFromTo(startIndex: Int, endIndex: Int): String {
+    var result = ""
+    forEachIndexed { index: Int, char: Char ->
+        if (index in startIndex..endIndex) {
+            result += char
+        }
+    }
+    return result
+}
+
+private fun String.findAllStartEndIndexes(start: String, end: String, countOfRepeat: Int): List<Pair<Int, Int>> {
+    val firstsIndexValue = this.allIndexOf(start)
+    val endsIndexValue = this.endsIndexOf(firstsIndexValue, start, end, countOfRepeat)
+    return combineListsToPair(firstsIndexValue, endsIndexValue)
+}
+
+private fun combineListsToPair(firstsIndexValue: List<Int>, endsIndexValue: List<Int>): List<Pair<Int, Int>>{
+    val mutableList = mutableListOf<Pair<Int, Int>>()
+    firstsIndexValue.forEachIndexed { index, i ->
+        mutableList.add(Pair(i, endsIndexValue[index]))
+    }
+    return mutableList
+}
+
+private fun String.endsIndexOf(
+    firstsIndexValue: List<Int>,
+    start: String,
+    end: String,
+    countOfRepeat: Int
+): List<Int> {
+    val mutableList = mutableListOf<Int>()
+    val negativeValue = if (end == "}") "{" else if (end == ")") "(" else ""
+    var countOfMeet = 0
+    var repeat = countOfRepeat
+    val i = 0
+    for (element in firstsIndexValue){
+        val indexOfStart = element + start.length -1
+         loop@ for (( index, char) in this.withIndex()){
+            if (index > indexOfStart) {
+                if (countOfMeet != -1) {
+                    if (countOfMeet <= -2) countOfMeet = 0
+                    if (char.toString() == end) {
+                        countOfMeet--
+                        if (index == this.length -1){
+                            mutableList.add(index)
+                        }
+                    } else if (char.toString() == negativeValue) {
+                        countOfMeet++
+                    }
+
+                } else {
+                    repeat--
+                    countOfMeet--
+
+                    if (repeat == 0){
+                        mutableList.add(index - 1)
+                        repeat = countOfRepeat
+                        break@loop
+                    }
+                }
+            }
+        }
+    }
+    return mutableList.toList()
+}
+
+private fun String.allIndexOf(value: String): List<Int> {
+    val mutableList = mutableListOf<Int>()
+    var index: Int = this.indexOf(value)
+    while (index >= 0) {
+        mutableList.add(index)
+        index = this.indexOf(value, index + 1)
+    }
+    return mutableList.toList()
 }
--- src/main/kotlin/TaculatorExtension.kt
+++ src/main/kotlin/TaculatorExtension.kt
@@ -10,4 +10,37 @@ object TaculatorExtension {
         }
         return result
     }
+
+
+    fun String.fracReplace(): Int {
+        val oldValue = "frac"
+
+
+        var occurrenceIndex: Int = indexOf(oldValue, 0)
+
+        return occurrenceIndex
+    }
+
+    fun String.replaceFrac(oldValue: String, newValue: String, ignoreCase: Boolean = false): String {
+        run {
+            var occurrenceIndex: Int = indexOf(oldValue, 0, ignoreCase)
+            // FAST PATH: no match
+            if (occurrenceIndex < 0) return this
+
+            val oldValueLength = oldValue.length
+            val searchStep = oldValueLength.coerceAtLeast(1)
+            val newLengthHint = length - oldValueLength + newValue.length
+            if (newLengthHint < 0) throw OutOfMemoryError()
+            val stringBuilder = StringBuilder(newLengthHint)
+
+            var i = 0
+            do {
+                stringBuilder.append(this, i, occurrenceIndex).append(newValue)
+                i = occurrenceIndex + oldValueLength
+                if (occurrenceIndex >= length) break
+                occurrenceIndex = indexOf(oldValue, occurrenceIndex + searchStep, ignoreCase)
+            } while (occurrenceIndex > 0)
+            return stringBuilder.append(this, i, length).toString()
+        }
+    }
 }
